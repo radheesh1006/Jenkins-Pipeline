2ec43d35779b93515a878ab35f3374fad08884c6
radheesh1006 <radheesh1006@gmail.com>
2025-07-02 21:39:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Hello from React!</h1>
+      <p>This is a very simple ReactJS app.</p>
+    </div>
+  );
+}
+
+export default App;
