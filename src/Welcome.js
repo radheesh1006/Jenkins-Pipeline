16cc9d4c87c2cf11415807c8e1aa38792e3d3ec8
radheesh1006 <159934100+radheesh1006@users.noreply.github.com>
2025-07-03 18:33:38 +0530

    Update Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Welcome({ name }) {
   return (
     <div>
-      <h1>👋 Welcome to React, {name}!</h1>
+      <h1>👋 Welcome to React Family, {name}!</h1>
       <p>This is a next-level React component with dynamic behavior.</p>
     </div>
   );
