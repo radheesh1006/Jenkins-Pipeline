22a358167ed1f2bb1fc7606dfce732b10e6da738
radheesh1006 <159934100+radheesh1006@users.noreply.github.com>
2025-07-03 14:42:15 +0530

    Create Welcome.js

---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Welcome({ name }) {
+  return (
+    <div>
+      <h1>👋 Welcome to React, {name}!</h1>
+      <p>This is a next-level React component with dynamic behavior.</p>
+    </div>
+  );
+}
+
+export default Welcome;
